980f08316b8625e9f6d299454b0f0ce3a409bcd9
amr21code <andi@noshut.de>
2022-06-08 19:52:27 +0200

    init

---
+++ philo/main.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anruland <anruland@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 17:43:27 by anruland          #+#    #+#             */
+/*   Updated: 2022/06/08 19:51:32 by anruland         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	if (ac < 5 || ac > 7)
+	{
+		ft_putstr("Usage: ");
+		ft_putstr(av[0]);
+		ft_putstr(" no_philos time_die(ms) time_eat(ms) time_sleep(ms");
+		ft_putstr(" [no_times_eat]\n");
+	}
+}
---
+++ philo/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anruland <anruland@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 17:41:29 by anruland          #+#    #+#             */
+/*   Updated: 2022/06/08 17:43:18 by anruland         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+#endif
